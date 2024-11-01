3ed9cd278ed64c5a3a168b4d0d36addef0a82b94
chris <c.f.oeser2@newcastle.ac.uk>
2024-11-01 12:35:56 +0000

    Added Search filter

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,11 +1,30 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import RecipeCard from './RecipeCard';
 import recipes from '@/data/recipes';
 
 const RecipeList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    // Filter recipes based on the search term
+    const filteredRecipes = recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <div className='recipe-list'>
-            {recipes.map((recipe) => (
+            {/* Search Input */}
+            <input
+                type='text'
+                placeholder='Search recipes...'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                style={{ padding: '8px', width: '100%', marginBottom: '20px'}} 
+            />
+            
+            {/* Display Recipes */}
+            {filteredRecipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
         </div>
