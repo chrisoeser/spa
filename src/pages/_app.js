10862e0a3110e7f75cce710680e929adecc0b8d4
chris <c.f.oeser2@newcastle.ac.uk>
2024-11-02 21:31:28 +0000

    Update Header and Favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
 import './globals.css';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+        <Head>
+            <title>Recipe Book</title>
+        </Head>
+        <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
